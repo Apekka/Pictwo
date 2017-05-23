73190f4e69c0a07828313b7d940e03e934912bbc
DoKuNu <dokunu@gmail.com>
2017-05-23 18:13:36 +0200

    Nick change

--- server/index.js
+++ server/index.js
@@ -1,15 +1,32 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users = [];
 
 io.origins('*:*');
 
 app.get('/', function(req, res){
+  res.sendFile(__dirname + '/nick.html');
+});
+
+app.get('/chat', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  var color = '#0b5de2';
+  socket.on('setNick', function(nick){
+      io.sockets.emit('setNick', {id: socket.id, nick: nick, color: color });
+      for (var i = 0; i < users.length; i++) {
+        if(users[i].id == socket.id) {
+          users[i].nick = nick;
+          break;
+        }
+      }
+      users.push({ id: socket.id, nick: nick, color: color });
+      console.log(nick + ' connected');
+      console.log(users);
+  })
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
