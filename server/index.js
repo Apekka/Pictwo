67ebe62884f40d9baadc3b02d9fd8029ba1e22db
DoKuNu <dokunu@gmail.com>
2017-05-30 16:46:39 +0200

    Chat et pictwo logo

--- server/index.js
+++ server/index.js
@@ -14,20 +14,29 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   var color = '#0b5de2';
-  var user = {id: socket.id, name: 'Dokunu', color: color}
-  socket.on('new-player', function(nick){
-      io.sockets.emit('', {id: socket.id, nick: nick, color: color });
+  var user;
+
+  socket.on('new-player', function(name){
+      var myName = name;
+      console.log(users);  
+      users.push({ id: socket.id, name: name, color: color });          
       for (var i = 0; i < users.length; i++) {
+        console.log("-----");        
+        console.log(users[i].id);
+        console.log("-----");
+        console.log(socket.id);
+        console.log("-----");
+      
         if(users[i].id == socket.id) {
-          users[i].nick = nick;
+          user = users[i];
           break;
         }
       }
-      users.push({ id: socket.id, nick: nick, color: color });
       io.sockets.emit('game-joined');
-      console.log(nick + ' connected');
-      console.log(users);
-  })
+      io.sockets.emit('special-message', {color : color, content: name + ' a rejoint'})
+      console.log(name + ' connected');
+      console.log(user);
+  });
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
